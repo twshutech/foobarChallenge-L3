802fc048ff1b6a73aaf628998bcfe867f8cfc52c
shuwei <38966382+twshutech@users.noreply.github.com>
2020-09-07 18:14:46 +0800

    update

--- matrixMapPath.py
+++ matrixMapPath.py
@@ -1,6 +1,7 @@
 def solution(prisonLayout):
   import copy
-
+  
+  
   class mapInfo:
     def __init__(self):
       self.floorMap = []
@@ -8,11 +9,10 @@ def solution(prisonLayout):
       self.floors = len(prisonLayout)
       self.doorsAndWalls = len(prisonLayout[0])
       self.pointsOfDoors = []
+      self.tree = [[0, 0]]
 
     def get_flip_layout(self):
       self.floorMap = copy.copy(prisonLayout)
-      # len(self.floorMap[0]) numbers of doors and walls in a floor.
-      # len(self.floorMap) numbers of floors of the prison.
       self.flipFloorMap = [[self.floorMap[j][i] for j in range(len(self.floorMap))] for i in range(len(self.floorMap[0]))]
       return self.flipFloorMap
 
@@ -21,16 +21,17 @@ def solution(prisonLayout):
       for i in range(len(floor)):
         if floor[i] == 0:
           p.append(i)
-      #('floor',floor,'p',p)
       return p
 
-    def get_doors(self):
-      level=0
+    def get_doors(self, level=0):
       for floor in self.floorMap:
         #print(floor)
+        #if level:
         door = self.horizontal_search(floor)
         location = [[level, door[i]]for i in range(len(door))]
+        #if location not in self.pointsOfDoors:
         self.pointsOfDoors = self.pointsOfDoors+location
+        print('self.pointsOfDoors',self.pointsOfDoors)
         level+=1
       return self.pointsOfDoors
 
@@ -41,13 +42,13 @@ def solution(prisonLayout):
       numberOfWalls = initWalls()
       for i in range(len(self.floorMap)):
         if yx[0] == yx[1]:
-          yx[1]+=1
+          yx[1]+=i
         elif yx[0]+1 == yx[1]:
-          yx[0]+=1
+          yx[0]+=i
         if self.floorMap[yx[0]][yx[1]]==1:
-          numberOfWalls[0]+=1
+          numberOfWalls[0]+=i
         if self.floorMap[yx[1]][yx[0]]==1:
-          numberOfWalls[1]+=1
+          numberOfWalls[1]+=i
       print(numberOfWalls)
 
     def straight_to_the_end(self):
@@ -65,28 +66,36 @@ def solution(prisonLayout):
           numberOfWalls[3]+=1
       print(numberOfWalls)
 
-    def ghost_way(self):
-      # Assume shortest way is m+n-1
-      shortCut = len(self.floorMap)+len(self.floorMap[0])-1
-      destination = [len(self.floorMap)-1, len(self.floorMap[0])-1]
+    # def ghost_way(self):
+    #   # Assume shortest way is m+n-1
+    #   shortCut = len(self.floorMap)+len(self.floorMap[0])-1
+    #   destination = [len(self.floorMap)-1, len(self.floorMap[0])-1]
+
+    # def treeBranches(self):
 
   class point:
     def __init__(self, x=0, y=0):
       self.coord = [y, x]
       self.compass = [[y-1, x], [y+1, x], [y, x-1], [y, x+1]] # up, down, left, right.
-  
+      self.penetrate = [[y-2, x], [y+2, x], [y, x-2], [y, x+2]] # penetrating wall causees 2 steps further. get coord to see if the location helps us get exit quicker.
+
   breadcrumbs = [[0, 0]]
   coord = mapInfo()
-  #print('get_doors',coord.get_doors(),coord.get_flip_layout())
-  for yx in coord.get_doors():
+
+  print('coord.get_doors()',coord.get_doors(),coord.pointsOfDoors)
+  for yx in coord.pointsOfDoors:
     pathInfo = point(yx[1], yx[0])
     for surrounding in pathInfo.compass:
-      if surrounding in coord.get_doors() and surrounding not in breadcrumbs:
+      try:
+        if coord.floorMap[surrounding[0]][surrounding[1]] == 1:
+          print('penetrate',pathInfo.penetrate)
+      except IndexError:
+        print('out of range')
+      if surrounding in coord.pointsOfDoors and surrounding not in breadcrumbs:
         breadcrumbs.append(surrounding)
-    #print(pathInfo.compass)
-    #print('bread',breadcrumbs,'length is',len(breadcrumbs))
-  coord.stair_to_the_end()
-  coord.straight_to_the_end()
+
+    print(pathInfo.compass)
+    print('bread',breadcrumbs,'length is',len(breadcrumbs))
 
 
 #solution([[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0]])

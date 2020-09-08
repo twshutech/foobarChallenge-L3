99cc2545e929facfeb40c9f26c4905d5813d48fd
shuwei kuo <shuweikuo@shuweideMacBook-Air.local>
2020-09-09 06:08:50 +0800

    - get tree from [the start , breakingpoint or turningpoinrt, endpoint]

--- matrixMapPath.py
+++ matrixMapPath.py
@@ -10,10 +10,68 @@ def solution(prisonLayout):
       self.pointsOfDoors = []
       self.pointsOfWalls = []
 
+    def validator(self, coord):
+      varified=False
+      if self.doorsAndWalls>coord[1]>=0 and self.floors>coord[0]>=0:
+        varified=True
+      return varified
+
+    def get_turning_point(self):
+      list_of_turning_point=[]
+      def turning_or_straight(coord):
+        numbers_of_doors=0
+        is_turning=False
+        movements=[]
+        coordInfo = point(coord[0], coord[1])
+        for w_or_d in coordInfo.compass:
+          if self.validator(w_or_d):
+            if self.floorMap[w_or_d[0]][w_or_d[1]] == 0:
+              numbers_of_doors+=1
+              movements.append(w_or_d)
+              if coord not in movements:
+                movements.append(coord)
+        for delta in movements:
+          delta_x=0
+          delta_y=0
+        delta_y = len([delta_ys for delta_ys in [[sort_y[0]]for sort_y in movements] if [[sort_y[0]]for sort_y in movements].count(delta_ys)==1])
+        delta_x = len([delta_xs for delta_xs in [[sort_x[1]]for sort_x in movements] if [[sort_x[1]]for sort_x in movements].count(delta_xs)==1])
+        if delta_x!=0 and delta_y!=0:
+          list_of_turning_point.append(coord)          
+
+      for door in self.pointsOfDoors:
+        turning_or_straight(door)
+      return list_of_turning_point
+
+    def linearCheck(self, arr, fromPoint):
+      def verify_linear_array(arr):
+        is_linear=True
+        for i in range(len(arr)):
+          # expect i = 0, 1, 2, 3, 4, 5
+          if arr[i] not in [i,fromPoint-i]:
+            is_linear=False
+            break
+        return is_linear
+
+      # def digging_deeper(moreArr):
+      #   if isinstance(arr[0])==True:
+      #     for subArr in moreArr:
+      #       digging_deeper(subArr)
+      #   else:
+      #     verify_linear_array(moreArr)
+
+      return verify_linear_array(arr)
+
     def horizontal_search(self, floor, finding):
       p = []
       for i in range(len(floor)):
         if floor[i] == finding:
+          p.append(i)
+      return p
+
+    def vertical_search(self, column, finding):
+      p = []
+      for i in range(len(column)):
+        if column[i] == finding:
           p.append(i)
       return p
 
@@ -58,8 +116,10 @@ def solution(prisonLayout):
       endPoint = [self.floors-1, self.doorsAndWalls-1]
       #print(endPoint[0]-leftVerticalStep,leftVerticalStep,endPoint)
       #printarr.append([endPoint[0]-j,endPoint[1]-i] for j in range(leftVerticalStep) for i in range(leftVHorizontalStep))
-      trimMap=[[endPoint[0]-j,endPoint[1]-i] for j in range(leftVerticalStep) for i in range(leftHorizontalStep) if [endPoint[0]-j,endPoint[1]-i] in self.pointsOfDoors]
-      trimMapy=[[endPoint[0]-j,endPoint[1]-i] for i in range(leftHorizontalStep) for j in range(leftVerticalStep) if [endPoint[0]-j,endPoint[1]-i] in self.pointsOfDoors]
+      # print(self.horizontal_search(self.floorMap[fromPoint[0]], 0))
+      # print(self.vertical_search(self.flipFloorMap[fromPoint[1]], 0))
+      trimMap=[[fromPoint[0]+j,fromPoint[1]+i] for j in range(leftVerticalStep) for i in range(leftHorizontalStep) if [fromPoint[0]+j,fromPoint[1]+i] in self.pointsOfDoors]
+      trimMapy=[[fromPoint[0]+j,fromPoint[1]+i] for i in range(leftHorizontalStep) for j in range(leftVerticalStep) if [fromPoint[0]+j,fromPoint[1]+i] in self.pointsOfDoors]
       if len(trimMapy)>len(trimMap):
         return trimMap
       else:
@@ -72,7 +132,7 @@ def solution(prisonLayout):
       for y in range(self.floors):
         for x in range(self.doorsAndWalls):
           compassPoint=point(y,x)
-          if [y,x] == breakPoint:
+          if [y,x] == breakPoint and [y,x] not in here_is_bread_for_record_x:
             here_is_bread_for_record_x.append([y,x])
             break
           else:
@@ -83,7 +143,7 @@ def solution(prisonLayout):
                 here_is_bread_for_record_x.append(item)
             #list_of_possible_space = [item for item in compassPoint.compass if compassPoint.getPoint_info(item,0)]
             #print('coord ',breakPoint ,'has list_of_possible_space',list_of_possible_space)
-        if [y,x] == breakPoint:
+        if breakPoint in here_is_bread_for_record_x:
           break
 
       for x in range(self.doorsAndWalls):
@@ -98,17 +158,25 @@ def solution(prisonLayout):
                 here_is_bread_for_record_y.append(item)
             #list_of_possible_space = [item for item in compassPoint.compass if compassPoint.getPoint_info(item,0)]
             #print('coord ',breakPoint ,'has list_of_possible_space',list_of_possible_space)
+        if breakPoint in here_is_bread_for_record_y:
+          break
       if len(here_is_bread_for_record_y)>len(here_is_bread_for_record_x):
         return here_is_bread_for_record_x
       else:
         return here_is_bread_for_record_y
 
+    def face_to(self, coord):
+      pointInfo = point(coord)
+
   class point:
     def __init__(self, y=0, x=0):
-      self.mapInfo = getMapInfo()
-      self.coord = [y, x]
-      self.compass = [[y-1, x], [y+1, x], [y, x-1], [y, x+1]] # up, down, left, right.
-      self.penetrate = [[y-2, x], [y+2, x], [y, x-2], [y, x+2]] # penetrating wall causees 2 steps further. get coord to see if the location helps us get exit quicker.
+      try:
+        self.mapInfo = getMapInfo()
+        self.coord = [y, x]
+        self.compass = [[y-1, x], [y+1, x], [y, x-1], [y, x+1]] # up, down, left, right.
+        self.penetrate = [[y-2, x], [y+2, x], [y, x-2], [y, x+2]] # penetrating wall causees 2 steps further. get coord to see if the location helps us get exit quicker.
+      except TypeError:
+        print('TypeError:')
 
     def getPoint_info(self, coord, finding):
       if coord[0]>=0 and coord[1]>=0 and coord[0]<self.mapInfo.floors and coord[1]<self.mapInfo.doorsAndWalls:
@@ -127,23 +195,46 @@ def solution(prisonLayout):
     mapInfo.get_walls()
     pointInfo = point(fromPoint[0],fromPoint[1])
     potentialBreakPoints=[]
+    endpoint = [mapInfo.floors-1, mapInfo.doorsAndWalls-1]
 
     def get_path_to_the_heaven():
+      def scanHV(turning_point):
+        print('turning_point',turning_point)
+        index_of_row = mapInfo.horizontal_search(mapInfo.floorMap[turning_point[1]], 0)
+        index_of_column = mapInfo.vertical_search(mapInfo.flipFloorMap[turning_point[0]], 0)
+        if mapInfo.linearCheck(index_of_column, turning_point[1])==True:
+          return
+          #print('linear x',[[turning_point[0],j] for j in index_of_column])
+        if mapInfo.linearCheck(index_of_row, turning_point[0])==True:
+          return
+          #print('linear y',[[i, turning_point[1]] for i in index_of_row])
+
       for try_this_wall in potentialBreakPoints:
-        print('length of breakpoint to endpoint',len(mapInfo.inspect_path(try_this_wall)))
-        print('path from breakpoint to endPoint',mapInfo.inspect_path(try_this_wall))
-        print('length of breakpoint to endpoint',len(mapInfo.path_to_breakpoint(try_this_wall)))
-        print('path from entrée_and_sortie:',mapInfo.path_to_breakpoint(try_this_wall))
-        # breadcrumbs = mapInfo.path_to_breakpoint(try_this_wall)+mapInfo.inspect_path(try_this_wall)
-        # print(len(breadcrumbs),'____is____',breadcrumbs)
+        # mapInfo.face_to(try_this_wall)
+        # print('length of breakpoint to endpoint',len(mapInfo.inspect_path(try_this_wall)))
+        reversed_path=mapInfo.inspect_path(try_this_wall)
+        reversed_path.sort(key=lambda  x:x[1])
+        # print('path from breakpoint to endPoint',reversed_path)
+        # print('length of breakpoint to endpoint',len(mapInfo.path_to_breakpoint(try_this_wall)))
+        # print('path from entree_and_sortie:',mapInfo.path_to_breakpoint(try_this_wall))
+        breadcrumbs = mapInfo.path_to_breakpoint(try_this_wall)+mapInfo.inspect_path(try_this_wall)
+        for turning_point in mapInfo.get_turning_point():
+          if turning_point in breadcrumbs:
+            scanHV(turning_point)
+            print('POSSIBILITY_TO_ESCAPE_FROM_HERE',turning_point)
+            if endpoint is mapInfo.horizontal_search(mapInfo.floorMap[turning_point[1]], 0) and mapInfo.vertical_search(mapInfo.flipFloorMap[turning_point[0]], 0):
+              print('________ESCAPED_________')
+
+        print(len(breadcrumbs),'____is____',breadcrumbs)
 
     def heaven_is_in_the_other_side_of_the_wall():
       for potentialBreakPoint in mapInfo.pointsOfWalls:
         the_wall_is_made_in_china = breakPoint(potentialBreakPoint[0], potentialBreakPoint[1])
-        if the_wall_is_made_in_china.ways_to_continue(potentialBreakPoint) == False:
+        if len(the_wall_is_made_in_china.ways_to_continue(potentialBreakPoint))>0:
+          print('ways have :',the_wall_is_made_in_china.ways_to_continue(potentialBreakPoint))
           potentialBreakPoints.append(potentialBreakPoint)
-          print('Heaven located in: [',potentialBreakPoint[0], potentialBreakPoint[1],']')
-      print('Numbers of potential heaven locations:',len(potentialBreakPoints),'which are:',potentialBreakPoints)
+          #print('Heaven located in: [',potentialBreakPoint[0], potentialBreakPoint[1],']')
+      #print('Numbers of potential heaven locations:',len(potentialBreakPoints),'which are:',potentialBreakPoints)
 
     def lets_walk():
       # print(mapInfo.floorMap)
@@ -156,13 +247,14 @@ def solution(prisonLayout):
             expectedBreakPoint = breakPoint(y,x)
             #print('point: [',y,x ,'] has num_of_walls',expectedBreakPoint.num_of_walls)
             #print('point: [',y,x ,']',len(expectedBreakPoint.list_of_walls),'list_of_walls',expectedBreakPoint.list_of_walls)
-            print('step to breakPoint is', step_counter,expectedBreakPoint.mapInfo.inspect_path([y,x]))
+            #print('step to breakPoint is', step_counter,expectedBreakPoint.mapInfo.inspect_path([y,x]))
             # expectedBreakPoint.ways_to_continue([for ])
       #       breadcrumbs.append([y,x])
       # print('breadcrumbs',breadcrumbs)
     lets_walk()
     heaven_is_in_the_other_side_of_the_wall()
     get_path_to_the_heaven()
+    print('list of turning point',mapInfo.get_turning_point())
 
   class breakPoint:
     def __init__(self, y=0, x=0):
@@ -178,17 +270,14 @@ def solution(prisonLayout):
         return 'shush no one knows'
 
     def ways_to_continue(self,coord):
-      oh_shit_more_walls=True
-      only_has_entrance_no_exit=0
+      oh_shit_more_walls=[]
       y=coord[0]
       x=coord[1]
       self.compass = [[y-1, x], [y+1, x], [y, x-1], [y, x+1]] # up, down, left, right.
       for continue_step in self.compass:
-        if str(self.getPoint_info(continue_step)) not in ['shush no one knows', '1']:
-          only_has_entrance_no_exit+=1
-          if only_has_entrance_no_exit>1:
-            oh_shit_more_walls=False
-            break
+        if str(self.getPoint_info(continue_step)) not in ['shush no one knows', '1'] and continue_step[0]>=0 and continue_step[1]>=0:
+          oh_shit_more_walls.append(continue_step)
+
       return oh_shit_more_walls
 
   # breadcrumbs = [[0, 0]]

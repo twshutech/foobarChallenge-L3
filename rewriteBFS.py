a0ebdedcbcf3af2a2444fa6416ce50f126d8108d
shuwei kuo <shuweikuo@shuweideMacBook-Air.local>
2020-12-03 07:14:03 +0800

    level

--- rewriteBFS.py
+++ rewriteBFS.py
@@ -1,44 +1,64 @@
 from collections import deque
 import queue
+p = [(1, 3), (3, 0), (5, 4), (2, 1), (5, 1), (0, 3), (4, 0), (1, 2), (3, 3), (4, 4), (5, 0), (2, 2), (4, 1), (1, 1), (3, 2), (0, 4), (1, 4), (2, 3), (4, 2), (1, 0), (5, 3), (0, 1), (5, 2), (3, 1), (0, 2), (2, 0), (4, 3), (3, 4), (2, 4)]
+l = [[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]]
+# for location in p:
+#   if l[location[0]][location[1]] == 0:
+#     l[location[0]][location[1]] = "X"
+lll = set()
+lll.add('1')
+print len(lll)
+lll.add('1')
+print len(lll)
 
-class coord:
-  def __init__(self, x, y):
-    self.x = x
-    self.y = y
-
+for row in l:
+  print row
 # Return left then right child.
-class get_children_nodes:
-  q = queue.Queue()
-  def children(self, x, y, q):
-    next_coord_y = coord(x,y+1)
-    next_coord_x = coord(x+1,y)
-    print 'next_coord_x',next_coord_x,'next_coord_y',next_coord_y
-    q.put(next_coord_y)
-    q.put(next_coord_x)
-    return self.q
+def get_children_nodes(x,y,xl,yl):
+  if y+1 < yl:
+    yield (x,y+1)
+  if x+1 < xl:
+    yield (x+1,y)
 
 def BFS(m, s, e, el):
+  set_l = set()
+  Q = queue.Queue()
   #Take from left, giving from right.
   q = deque()
   #Insert start point.
   q.append(s)
   #Get coming children.
-  c = get_children_nodes()
-
+  count = 0
   while len(q)>0:
+    # print 'q',q
     node = q.popleft()
-    two_children = c.children(node.x, node.y)
-    xxx = two_children.queue
-    
-    print 'two_children',xxx
+    #print '--node--',node
+    if el[node[0]][node[1]] == 'X':
+      el[node[0]][node[1]] = count
+      count+=1
+    if node[0] != len(m[0]) and node[1] != len(m): #Current node isnt the exit.
+      two_children = get_children_nodes(node[0], node[1],len(m[0]), len(m))
+      for i in two_children:
+        if i not in set_l:
+          set_l.add(i)
+          Q.put(i)
+          q.append(i)
+
+        if m[node[0]][node[1]] == 0:
+          m[node[0]][node[1]] = '_'
+  # print 'set_l',set_l,len(set_l)
+  print 'Q',Q.queue
+  # for row in el:
+  #   print el
+  # print 'm',m
+  # for row in m:
+  #   print row
 
 if __name__ == '__main__':
   to_null = lambda a:'X'
   empty_l = []
-  l = [[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]]
   for row in l:
     empty_l.append(map(to_null, row))
-  print 'empty_l'
-  s = coord(0,0)
-  e = coord(len(l[0]),len(l))
+  s = (0,0)
+  e = (len(l[0]),len(l))
   BFS(l, s, e, empty_l)
---
+++ safekey.py
@@ -0,0 +1,87 @@
+def solution(m):
+
+    # Calculate map stats
+    w, h = len(m[0]), len(m) # width and height
+    
+    # The current minimal solution
+    s_min = 401
+
+    # Iterate over all possible inputs (by replacing 1s with 0s).
+    for m_0 in all_maps(m):
+        print 'm_0',m_0
+        # Find the minimal path length
+        s_min = min(min_path(m_0, w, h), s_min)
+
+        # Optimization: Minimal solution?
+        if s_min == w + h - 1:
+            return s_min
+        
+    return s_min
+
+
+def min_path(m, w, h):
+    '''Takes a map m and returns the minimal path
+    from the start to the end node. Also pass width and height.
+    '''
+        
+    # Initialize dictionary of path lengths
+    # integer: {(i,j), ...} (Set of nodes (i,j) with this integer path length)
+    d = {1: {(0,0)}}
+
+    # Expand "fringe" successively
+    path_length = 2
+    while path_length < 401 and d[path_length-1]:
+
+        # Fill fringe
+        fringe = set()
+        for x in d[path_length-1]:
+            
+            # Expand node x (all neighbors) and exclude already visited
+            expand_x = {y for y in neighbors(x,m) if not any(y in visited for visited in d.values())}
+            fringe = fringe | expand_x
+            
+        # Have we found min path of exit node?
+        if (h-1, w-1) in fringe:
+            return path_length
+        
+        # Store new fring of minimal-path nodes
+        d[path_length] = fringe
+
+        # Find nodes with next-higher path_length
+        path_length += 1
+
+    return 401 # Infinite path length  
+        
+
+def neighbors(x, m):
+    '''Returns a set of nodes (as tuples) that are neighbors of node x in m.'''
+    i, j = x
+    w, h = len(m[0]), len(m)
+    candidates = {(i-1,j), (i+1,j), (i,j-1), (i,j+1)}
+    neighbors = set()
+    for y in candidates:
+        i, j = y
+        if i>=0 and i<h and j>=0 and j<w and m[i][j] == 0:
+            neighbors.add(y)
+    
+    return neighbors
+
+
+def all_maps(m):
+    '''Returns an iterator for memory efficiency
+    over all maps that arise by replacing a '1' with a '0' value.'''
+
+    # Unchanged map is a valid solution
+    yield m
+
+    for i in range(len(m)):
+        for j in range(len(m[i])):
+            if m[i][j]:
+                # Copy the map
+                copy = [[col for col in row] for row in m]
+
+                # Replace 1 by 0 and yield new map
+                copy[i][j] = 0
+                yield copy
+l = [[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]]
+print solution(l)

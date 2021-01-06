990eab70f356943602a061c017dad066e7add346
develop <develop@shuweideMacBook-Pro.local>
2021-01-07 07:27:30 +0800

    verify for recrusion

--- foobar3-2.py
+++ foobar3-2.py
@@ -1,26 +1,48 @@
 # the bombs self-replicate via one of two distinct processes:
 # Every Mach bomb retrieves a sync unit from a Facula bomb; for every Mach bomb, a Facula bomb is created;
 # Every Facula bomb spontaneously creates a Mach bomb.
-print (1,0,244)
+def check_numbers_retrieves(ns,steps=0):
+  solu = dict({'ns':ns, 'steps':steps})
+  delta_steps = lambda a,b:a/b
+  rest_bombs = lambda a,b:a%b
+
+  if max(ns) != min(ns):
+    modified_bomb = rest_bombs(max(ns), min(ns))
+    steps += delta_steps(max(ns), min(ns))
+
+    if modified_bomb != 0:
+      ns = (modified_bomb, min(ns))[::-1]
+    else:
+      modified_bomb += min(ns)
+      steps -= 1
+      ns = (modified_bomb, min(ns))
+    solu['ns'] = ns
+    solu['steps'] = steps
+  return solu
 
 def first_process(m, f):
   return [(m+f, f), (m, m+f)]
 
 def second_process(n, delta_n):
+  print 'n',n,'delta_n',delta_n
   n = int(n)
   delta_n = int(delta_n)
-
-  return divmod(n, delta_n)
+  if delta_n == 0 or n == 0:
+    return (0, n)
+  else:
+    return n - delta_n * (n/delta_n)
 
 def is_this_fibonacci(bomb_set):
+
+  bomb_set = sorted(bomb_set)
   message = {
     'count': 0,
     'solution': '',
     'mf': bomb_set
   }
-
+  print 'bomb_set',bomb_set
   while message['mf'] != (1,1) or message['solution'] != "impossible":
-    result = second_process(*bomb_set)
+    result = second_process(*bomb_set[::-1])
 
     if cmp(*message['mf']) == 0:
       message['solution'] = "impossible"
@@ -34,21 +56,32 @@ def is_this_fibonacci(bomb_set):
 
     if message['mf'] == (1,1) or message['solution'] == "impossible":
       break
-  return message
+  return bomb_set
+
+# def is_solution(mf):
+#   if mf[]
+#   if mf == (1,1):
+#     return True
+#   elif mf[0] == 0 or mf[1] == 0:
+#     return 
+#   verify the current bombs state to find solution
+
 
 def solution(m,f):
   depth = []
   processes = first_process(int(m), int(f))
   for bomb_set in processes:
-    mf = sorted(bomb_set)
-    depth.append(is_this_fibonacci(mf))
-  answer = ""
-  if isinstance(answer['count'], int):
-     answer = str(answer['count'])
-  return answer
+    steps = 0
+    #depth.append(is_this_fibonacci(bomb_set))
+    l = sorted(bomb_set, reverse=True)
+    mf = check_numbers_retrieves(l, steps)
+    depth.append(mf)
+    # while mf['ns'] != (1,1) or f:
 
-print "('2', '1')",solution('2', '1'),"\n"
-print "('2', '4')",solution('2', '4'),"\n"
-print "('4', '7')",solution('4', '7'),"\n"
-print "('2', '2')",solution('2', '2'),"\n"
-print "('149333', '12')",solution('149333','12')
+  return depth
+
+print "('2', '1'):\n",solution('2', '1'),"\n"
+print "('2', '4'):\n",solution('2', '4'),"\n"
+print "('4', '7'):\n",solution('4', '7'),"\n"
+print "('2', '2'):\n",solution('2', '2'),"\n"
+print "('149333', '12'):\n",solution('149333','12')

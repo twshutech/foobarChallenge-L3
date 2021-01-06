48a9d743522c9f37687e14e909cfb128b1bb3d8e
Josh Kuo <Josh.Kuo@dlinkcorp.com>
2021-01-06 17:26:03 +0800

    orgnize func

--- foobar3-2.py
+++ foobar3-2.py
@@ -1,7 +1,7 @@
 # the bombs self-replicate via one of two distinct processes:
 # Every Mach bomb retrieves a sync unit from a Facula bomb; for every Mach bomb, a Facula bomb is created;
 # Every Facula bomb spontaneously creates a Mach bomb.
-print divmod(2, 1)
+print (1,0,244)
 
 def first_process(m, f):
   return [(m+f, f), (m, m+f)]
@@ -10,35 +10,39 @@ def second_process(n, delta_n):
   n = int(n)
   delta_n = int(delta_n)
 
-  print n,delta_n
   return divmod(n, delta_n)
 
-def is_this_fibonacci(m,f):
+def is_this_fibonacci(bomb_set):
   message = {
     'count': 0,
     'solution': '',
-    'mf': tuple()
+    'mf': bomb_set
   }
-  processes = first_process(int(m), int(f))
-  for bomb_set in processes:
-    while message['mf'] != (1,1) or message['solution'] != "impossible":
-      result = second_process(*bomb_set)
-      message['count'] += result[0]
-      bomb_set = (result[1], bomb_set[1])
-      message['mf'] = bomb_set
-      print 'result',result,'bomb_set',bomb_set,cmp(*message['mf'])
 
-      if cmp(*message['mf']) == 0:
-        message['solution'] = "impossible"
-      elif cmp(*message['mf']) == -1:
-        bomb_set = bomb_set[::-1]
+  while message['mf'] != (1,1) or message['solution'] != "impossible":
+    result = second_process(*bomb_set)
 
-      if message['mf'] == (1,1) or message['solution'] == "impossible":
-        break
+    if cmp(*message['mf']) == 0:
+      message['solution'] = "impossible"
+    elif cmp(*message['mf']) == -1:
+      bomb_set = bomb_set[::-1]
+
+    message['count'] += result[0]
+    bomb_set = (result[1], bomb_set[1])
+    message['mf'] = bomb_set
+    print 'result',result,'bomb_set',bomb_set,cmp(*message['mf'])
+
+    if message['mf'] == (1,1) or message['solution'] == "impossible":
+      break
   return message
 
 def solution(m,f):
-  answer = is_this_fibonacci(m, f)
+  depth = []
+  processes = first_process(int(m), int(f))
+  for bomb_set in processes:
+    mf = sorted(bomb_set)
+    depth.append(is_this_fibonacci(mf))
+  answer = ""
   if isinstance(answer['count'], int):
      answer = str(answer['count'])
   return answer
